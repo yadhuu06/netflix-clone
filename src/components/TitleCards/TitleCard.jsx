e1f9a3657ccfbe1ed7aa9b1b1ba01a187ee185a1
yadhu <yadhupaikkattu3232@gmail.com>
2025-02-10 12:51:37 +0530

    Fetch movies from TMDB API and display in TitleCards

--- src/components/TitleCards/TitleCard.jsx
+++ src/components/TitleCards/TitleCard.jsx
@@ -1,26 +1,46 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
 
 const TitleCard = ({title,category}) => {
-  const cardsRef = useRef()
+  const [apiData,setApiData]=useState([]);
+  const cardsRef = useRef();
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMWY1MzAxNTIzYjM2ZDFjOWUxMzYwNTA4NzhhNmE3NiIsIm5iZiI6MTczOTE2NzA4OC40NTIsInN1YiI6IjY3YTk5NTcwMmZiYWM0MGI5MDkzNzBhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vpLIrjaFfbFw7hodB8_F2kjAhzbqulzSQAt-bFFK2wo'
+    }
+  };
+  
+  
+    
+  
+  
+
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   }
 
-  useEffect(() => { cardsRef.current.addEventListener('wheel', handleWheel) }, [])
+  useEffect(() => { 
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+    
+    cardsRef.current.addEventListener('wheel', handleWheel) }, [])
 
   return (
     <div className='TitleCards'>
       <h2>{title?title:"New Movies"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return <div className='card' key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={'https://image.tmdb.org/t/p/w500'+ card.backdrop_path} alt="" />
+            <p>{card.original_title}</p> 
           </div>
         })}
       </div>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 
@@ -6,30 +6,49 @@ import logo from '../../assets/logo.png'
 
 
 function Login() {
+  const [signState, setSignState] = useState("Sign In")
   return (
     <div className='login'>
-      <img src={logo} className="login-logo"alt="" />
+      <img src={logo} className="login-logo" alt="" />
       <div className="login-form">
         <h1>Sign UP</h1>
         <form >
-          <input type="text" placeholder='your Name' />
+          {signState === "Sign Up" ? <input type="text" placeholder='your Name' /> : <></>}
+
           <input type="text" placeholder='Enter The Email' />
           <input type="text" placeholder='Enter The Password' />
-          <button>Sign Up</button>
+          <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
               <label htmlFor="">Remember me</label>
-
             </div>
             <p>Need help?</p>
           </div>
-
-
-
         </form>
+        <div className="form-switch">
+  {signState === "Sign In" ? (
+    <p>
+      New to Netflix?{" "}
+      <span onClick={() => setSignState("Sign Up")} style={{ color: "#e50914", cursor: "pointer" }}>
+        Sign Up Now
+      </span>
+    </p>
+  ) : (
+    <p>
+      Already have an account?{" "}
+      <span onClick={() => setSignState("Sign In")} style={{ color: "#e50914", cursor: "pointer" }}>
+        Sign In now
+      </span>
+    </p>
+  )}
+</div>
+
+
+
+
+
       </div>
-      
     </div>
   )
 }
